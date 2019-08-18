53d171d7481ba9496191123b032c8cb2d5033b4f
Varun Setia <vsetia46@gmail.com>
2019-08-18 21:07:59 +0530

    UI enhancements

--- ng-source/portfolio-app/src/app/components/navigation/navigation.component.ts
+++ ng-source/portfolio-app/src/app/components/navigation/navigation.component.ts
@@ -7,41 +7,61 @@ import { NavigationItem } from './navigationItem';
   styleUrls: ['./navigation.component.css']
 })
 export class NavigationComponent implements OnInit {
+
   navigationItems: NavigationItem[];
+  isNavExpanded: boolean;
 
   constructor() {
     this.populateNav();
+    this.isNavExpanded = false;
   }
 
   private populateNav() {
     this.navigationItems = [];
     const navItem1: NavigationItem = new NavigationItem();
     navItem1.RouteName = 'Home';
-    navItem1.RouteLinkPathName = '';
+    navItem1.RouteLinkPathName = '#welcome-page';
     this.navigationItems.push(navItem1);
+    const navItem3: NavigationItem = new NavigationItem();
+    navItem3.RouteName = 'Technology Stack';
+    navItem3.RouteLinkPathName = '#technology-stack-items';
+    this.navigationItems.push(navItem3);
     const navItem2: NavigationItem = new NavigationItem();
     navItem2.RouteName = 'Portfolio';
-    navItem2.RouteLinkPathName = 'portfolio';
+    navItem2.RouteLinkPathName = '#home-github-repo-item';
     this.navigationItems.push(navItem2);
-    const navItem3: NavigationItem = new NavigationItem();
-    navItem3.RouteName = 'About this website';
-    navItem3.RouteLinkPathName = 'aboutwebsite';
-    this.navigationItems.push(navItem3);
-    this.activeLink('Home');
   }
 
-  public activeLink(name: string) {
-    this.navigationItems.forEach((value, index) => {
-      if (value.RouteName === name) {
-        this.navigationItems[index].IsActive = true;
-      } else {
-        this.navigationItems[index].IsActive = false;
-      }
-    });
-
-  }
 
   ngOnInit() {
   }
+
+
+  public toggleNavigationExpander() {
+
+    const selectedBar = document.querySelector('#navigation-expander').querySelectorAll('.bar');
+    const navItemsList = document.querySelector('#navitem-list');
+
+    if (this.isNavExpanded) {
+      selectedBar.forEach(item => {
+        item.classList.remove('open-bar');
+      });
+      navItemsList.classList.add('hide');
+      this.isNavExpanded = false;
+
+    } else {
+      selectedBar.forEach(item => {
+      item.classList.add('open-bar');
+    });
+    navItemsList.classList.remove('hide');
+    this.isNavExpanded = true;
+
+    }
+  }
+
+
+
+
+
 
 }
