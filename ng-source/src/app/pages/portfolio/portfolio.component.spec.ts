f5bf091dc596a3205ed1c2cd6d834c293c0f991d
VARUN46 <vsetia46@gmail.com>
2021-10-03 16:38:44 +0530

    Test cases updated

--- ng-source/src/app/pages/portfolio/portfolio.component.spec.ts
+++ ng-source/src/app/pages/portfolio/portfolio.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { PortfolioComponent } from './portfolio.component';
 
@@ -8,6 +9,7 @@ describe('PortfolioComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [ HttpClientTestingModule ],
       declarations: [ PortfolioComponent ]
     })
     .compileComponents();
