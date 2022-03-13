83ea85208e43f9f4f69a8ba59d29e69933eb9e1d
VARUN46 <vsetia46@gmail.com>
2022-03-13 12:30:42 +0530

    Repo Items

--- react-source/react-app/src/components/github-card/github-card.js
+++ react-source/react-app/src/components/github-card/github-card.js
@@ -1,26 +1,30 @@
 import './github-card.css';
 import axios from 'axios';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import GithubCardItem from './github-card-item'; 
 
 function GithubCard(){
 
-    let cardItemsArray = [];
+    const [cardItemsArray, setCardItemsArray] = useState([]);
     useEffect(() => {
     
-        axios.get('https://api.github.com/users/varun46/repos')
-        .then(response=>{
+    axios.get('https://api.github.com/users/varun46/repos')
+        .then(response=>{     
+            const responseCollection = [];     
             response.data.forEach(githubRepoItem=>{
-                cardItemsArray.push(<GithubCardItem title={githubRepoItem.full_name} description={githubRepoItem.description} />);
-            });
+                responseCollection.push({ full_name:githubRepoItem.full_name, description: githubRepoItem.description });
         });
+        setCardItemsArray(responseCollection);
+    });
 });
     return (     
     
     <div className="container">
     <div className="github-portfolio">
         <h3>Github Portfolio</h3>
-        {cardItemsArray}
+        {cardItemsArray.map(item=>{
+            return <GithubCardItem key={item.id} title={item.full_name} description={item.description} />
+        })}
     </div>
 </div>);
 }
