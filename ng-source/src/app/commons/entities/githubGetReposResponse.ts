882e84bab653c0a26cf44f6139081eb3cba0b9e2
VARUN46 <vsetia46@gmail.com>
2022-04-15 14:07:03 +0530

    Removed angular code

--- ng-source/src/app/commons/entities/githubGetReposResponse.ts
+++
@@ -1,6 +0,0 @@
-export class githubGetReposResponse{
-  name: string = '';
-  html_url: string = '';
-  fork: boolean = false;
-  description: string = '';
-}
--- ng-source/src/app/commons/services/loader/loader.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@angular/core';
-
-@Injectable({
-  providedIn: 'root'
-})
-export class LoaderService {
-  SHOW_LOADER: boolean = true;
-  constructor() { }
-}
