08f6a9af3f4a82b214b4548d3ac98a873c2d0de1
VARUN46 <vsetia46@gmail.com>
2024-07-07 13:15:21 +0530

    ui enhacements

--- src/portfolio/src/App.js
+++ src/portfolio/src/App.js
@@ -16,25 +16,21 @@ function App() {
 
   return (
     <div className="App">
-      <div className="container-fluid text-center">
-  <div className="row">
+  <div className="container-content">
     <div className="navigation-header">
     <GiHamburgerMenu onClick={ () => { setShowNav(true); }} className='nav-show-action' />
 
     </div>
-    { showNav && <div className="navigation">
+    <div className={`navigation ${showNav ? 'navigation-visible' : ''}` }>
     <IoMdClose onClick={ () => { setShowNav(false); }} />
 
       <button onClick={ () => navigateTo(1)} className="navigation-link">Introduction</button>
       <button onClick={ () => navigateTo(2)} className="navigation-link">DSA Articles</button>
-    </div> }
-    <div className="col-12">
+    </div>
     <div className="content-wrapper">
     { navigationState === 1 && <Intro></Intro> }
     { navigationState === 2 && <Dsa></Dsa> }
     </div>
-    </div>
-  </div>
 </div>
       
     </div>
