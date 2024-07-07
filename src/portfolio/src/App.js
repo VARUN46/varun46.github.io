de33e25cc5c3d713c186548b7797230830bd8fe7
VARUN46 <vsetia46@gmail.com>
2024-07-07 12:52:55 +0530

    Navigation bar added

--- src/portfolio/src/App.js
+++ src/portfolio/src/App.js
@@ -2,13 +2,15 @@ import './App.css';
 import Intro from './layouts/Intro';
 import Dsa from './layouts/Dsa';
 import { useState } from 'react';
-
+import { IoMdClose } from "react-icons/io";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 function App() {
 
   const [navigationState, setNavigationState] = useState(1);
+  const [showNav, setShowNav] = useState(false);
 
-  const navigateTo = (stateVal)=>{
+  const navigateTo = (stateVal) => {
     setNavigationState(stateVal);
   }
 
@@ -16,13 +18,21 @@ function App() {
     <div className="App">
       <div className="container-fluid text-center">
   <div className="row">
-    <div className="col-2 navigation">
-      <button onClick={()=>navigateTo(1)} className="navigation-link">Introduction</button>
-      <button onClick={()=>navigateTo(2)} className="navigation-link">DSA Articles</button>
+    <div className="navigation-header">
+    <GiHamburgerMenu onClick={ () => { setShowNav(true); }} className='nav-show-action' />
+
     </div>
-    <div className="col-10">
+    { showNav && <div className="navigation">
+    <IoMdClose onClick={ () => { setShowNav(false); }} />
+
+      <button onClick={ () => navigateTo(1)} className="navigation-link">Introduction</button>
+      <button onClick={ () => navigateTo(2)} className="navigation-link">DSA Articles</button>
+    </div> }
+    <div className="col-12">
+    <div className="content-wrapper">
     { navigationState === 1 && <Intro></Intro> }
     { navigationState === 2 && <Dsa></Dsa> }
+    </div>
     </div>
   </div>
 </div>
--- src/portfolio/src/layouts/Dsa.js
+++ src/portfolio/src/layouts/Dsa.js
@@ -10,12 +10,14 @@ function Dsa(){
         setBlogPage([<DsaNArraySumZero></DsaNArraySumZero>])
     }
 
-    return (<>{blogPage.length>0 && <div className='blog-page'>{blogPage}<h4>Happy Coding !!</h4></div> }
-      <div className="dsa-intro-page">
+    return (<>
+      {blogPage.length > 0 && <div className='blog-page'>{blogPage}<h4>Happy Coding !!</h4></div> }
+      {blogPage.length === 0 && <div className="dsa-intro-page">
         <h2>Welcome to my DSA problems and solution</h2>
         <p>Below are a few tricky problems, they might help you crack the interview.</p>
         <button onClick={()=> openArticle()} className='navigation-link'>Given a value of N, create an array of N such that it results to zero.</button>
-        </div></>);
+        </div>}
+        </>);
 }
 
 export default Dsa;
